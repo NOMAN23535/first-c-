2342e9634a7d7d765450acf219545167eb23a60c
NOMAN HAMEED <k13503099080@gmail.com>
2024-12-04 17:01:12 -0800

    Add files via upload

---
+++ Subject marks 5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int marks[5],i;
+	 cout<<"Enter Five Subjects markse one by one"<<endl;
+	 for(i=0; i<5;i++)
+	 {
+	   cin>>marks[i];	
+	 }
+	 cout<<"Your Subjects Marks are"<<endl;
+	 for(i=0;i<5;i++)
+	 {
+	 
+	 cout<<marks[i]<<endl;
+ }
+	 return 0;
+}
